354c9179ccfdd7ccfd74cfbe4f347009e014569a
Tola200010 <Tola.cambodia000@gmail.com>
2022-08-05 17:01:33 +0700

    Apply Clean Architecture for Ordering API

---
+++ src/Services/Ordering/Ordering.Domian/Common/EntryBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domian.Common
+{
+    public abstract class EntryBase
+    {
+        public int Id { get;protected set; }
+        public string? CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
